4a198a6e1333629a7f4cc70d06c124982049df06
VenkatDevOps-96 <kamisettyvenkatesh96@gmail.com>
2025-12-31 00:14:37 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,6 +11,15 @@ locals {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
