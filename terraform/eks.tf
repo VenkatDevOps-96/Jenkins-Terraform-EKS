0f385b53cde204f384075fdbfb952b4c8d22c520
VenkatDevOps-96 <kamisettyvenkatesh96@gmail.com>
2025-12-31 00:47:17 +0530

    Refactor EKS node group and addon configurations
    Updated EKS configuration to simplify node group definition and ensure consistent addon settings.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -6,37 +6,26 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+    coredns   = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni   = { most_recent = true }
   }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2023_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
-
   eks_managed_node_groups = {
-      cluster-wg = {
+    cluster-wg = {
+      ami_type       = "AL2023_x86_64"
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      attach_cluster_primary_security_group = true
+
       min_size     = 1
       max_size     = 2
       desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
 
       tags = {
         ExtraTag = "helloworld"
